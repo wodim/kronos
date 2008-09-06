wodim/kronos

commit 7c0b0ce902ceb9b04b6483f3637d46fbb6bfb683

quick fix
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2406 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/kernel/kvi_app.cpp
@@ -437,12 +437,6 @@ void KviApp::setup()
 
 	KVI_SPLASH_SET_PROGRESS(88)
 
-	KviInputHistory::init();
-	if(getReadOnlyConfigPath(tmp,KVI_CONFIGFILE_INPUTHISTORY))
-		KviInputHistory::instance()->load(tmp);
-
-	KVI_SPLASH_SET_PROGRESS(89)
-
 	KviAvatarCache::init();
 	if(getReadOnlyConfigPath(tmp,KVI_CONFIGFILE_AVATARCACHE))
 		KviAvatarCache::instance()->load(tmp);

src/kvirc/ui/kvi_topicw.h
@@ -29,7 +29,6 @@
 #include "kvi_string.h"
 #include "kvi_app.h"
 #include "kvi_input_editor.h"
-//#include "kvi_input.h"
 #include "kvi_tal_listwidget.h"
 #include "kvi_tal_itemdelegates.h"
 

commit 142f53a3ec0971dddf50234e7576742a42851db9

fix for a small problem introduced in r2406
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2407 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/kernel/kvi_app.cpp
@@ -441,6 +441,12 @@ void KviApp::setup()
 	if(getReadOnlyConfigPath(tmp,KVI_CONFIGFILE_AVATARCACHE))
 		KviAvatarCache::instance()->load(tmp);
 
+	KVI_SPLASH_SET_PROGRESS(89)
+
+	KviInputHistory::init();
+	if(getReadOnlyConfigPath(tmp,KVI_CONFIGFILE_INPUTHISTORY)) 
+		KviInputHistory::instance()->load(tmp); 
+
 	KVI_SPLASH_SET_PROGRESS(90)
 
 	// Eventually initialize the crypt engine manager