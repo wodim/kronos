wodim/kronos

commit 691437760d1930928335855ad8fd826ea6dcb7a7

- Don't crash if an escape sequence contains a newline (or the end of the buffer). - Nick completion now attempts to escape "crazy" nicknames when it's performed inside a command - Improved the efficiency of some logging-related code - Fixed a couple of typos
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4265 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/ui/kvi_ircview_loghandling.cpp
@@ -246,17 +246,47 @@ bool KviIrcView::startLogging(const QString& fname,bool bPrependCurBuffer)
 	return true;
 }
 
-void KviIrcView::add2Log(const QString &szBuffer,int iMsgType)
+
+void KviIrcView::add2Log(const QString &szBuffer,int iMsgType,bool bPrependDate)
 {
 	QByteArray szTmp;
-	QString szToWrite;
-	if(KVI_OPTION_BOOL(KviOption_boolStripMsgTypeInLogs))
+
+	if(!KVI_OPTION_BOOL(KviOption_boolStripMsgTypeInLogs))
 	{
-		szToWrite = QString("%1\n").arg(szBuffer);
-	} else {
-		szToWrite = QString("%1 %2\n").arg(iMsgType).arg(szBuffer);
+		QString szMessageType = QString("%1 \n").arg(iMsgType);
+
+		szTmp = KviQString::toUtf8(szMessageType);
+
+		if(m_pLogFile->write(szTmp.data(),szTmp.length())==-1)
+			debug("WARNING : Can not write to the log file.");
 	}
-	szTmp = KviQString::toUtf8(szToWrite);
+	
+	if(bPrependDate)
+	{
+		QString szDate;
+		QDateTime date = QDateTime::currentDateTime();
+		switch(KVI_OPTION_UINT(KviOption_uintOutputDatetimeFormat))
+		{
+			case 0:
+				szDate = date.toString("[hh:mm:ss] ");
+			break;
+			case 1:
+				szDate = date.toString(Qt::ISODate);
+			break;
+			case 2:
+				szDate = date.toString(Qt::SystemLocaleDate);
+			break;
+		}
+
+		szTmp = KviQString::toUtf8(szDate);
+
+		if(m_pLogFile->write(szTmp.data(),szTmp.length())==-1)
+			debug("WARNING : Can not write to the log file.");
+	}
+	
+	szTmp = KviQString::toUtf8(szBuffer);
+
 	if(m_pLogFile->write(szTmp.data(),szTmp.length())==-1)
 		debug("WARNING : Can not write to the log file.");
 }
+

src/kvirc/ui/kvi_input_editor.cpp
@@ -1712,23 +1712,25 @@ void KviInputEditor::getWordBeforeCursor(QString & szBuffer, bool * bIsFirstWord
 void KviInputEditor::completion(bool bShift)
 {
 	// FIXME: Spaces in directory completion can mess everything completely
-	//        On windows the KVI_PATH_SEPARATOR_CHARacters are breaking everything...
+	//        On windows the characters are breaking everything...
 	//        Well.... :D
 
 	QString szWord;
 	QString szMatch;
 	bool bFirstWordInLine;
 
+	bool bInCommand = m_szTextBuffer.trimmed().indexOf('/') == 0;
+
 	getWordBeforeCursor(szWord,&bFirstWordInLine);
 	if(szWord.isEmpty())
 	{
-		if(m_szLastCompletedNick.isEmpty()) return; // nothing to complete
-		else {
-			// this is standard nick completion continued
-			standardNickCompletion(bShift,szWord,bFirstWordInLine);
-			repaintWithCursorOn();
-			return;
-		}
+		if(m_szLastCompletedNick.isEmpty())
+			return; // nothing to complete
+
+		// this is standard nick completion continued
+		standardNickCompletion(bShift,szWord,bFirstWordInLine,bInCommand);
+		repaintWithCursorOn();
+		return;
 	}
 	int iOffset;
 	if(KviQString::equalCI(m_szTextBuffer.left(5),"/help") || KviQString::equalCI(m_szTextBuffer.left(5),"/help.open")) iOffset=1;
@@ -1811,7 +1813,7 @@ void KviInputEditor::completion(bool bShift)
 					bIsNick = true;
 					m_szLastCompletedNick=szWord;
 				} else {
-					standardNickCompletion(bShift,szWord,bFirstWordInLine);
+					standardNickCompletion(bShift,szWord,bFirstWordInLine,bInCommand);
 					repaintWithCursorOn();
 					return;
 				}
@@ -1820,7 +1822,7 @@ void KviInputEditor::completion(bool bShift)
 				m_pUserListView->completeNickBashLike(szWord,&tmp,bShift);
 				bIsNick = true;
 			} else {
-				standardNickCompletion(bShift,szWord,bFirstWordInLine);
+				standardNickCompletion(bShift,szWord,bFirstWordInLine,bInCommand);
 				repaintWithCursorOn();
 				return;
 			}
@@ -1842,6 +1844,11 @@ void KviInputEditor::completion(bool bShift)
 				{
 					if(bFirstWordInLine || (!KVI_OPTION_BOOL(KviOption_boolUseNickCompletionPostfixForFirstWordOnly)))
 						szMatch.append(KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix));
+				}
+				if(bInCommand)
+				{
+					// escape crazy things like Nick\nquit
+					szMatch.replace(QChar('\\'),QString::fromAscii("\\\\"));
 				}
 			}
 		} else {
@@ -1884,28 +1891,50 @@ void KviInputEditor::completion(bool bShift)
 	repaintWithCursorOn();
 }
 
-void KviInputEditor::replaceWordBeforeCursor(const QString & szWord, const QString & szRreplacement, bool bRepaint)
+void KviInputEditor::replaceWordBeforeCursor(const QString & szWord, const QString & szReplacement, bool bRepaint)
 {
 	selectOneChar(-1);
 	m_iCursorPosition -= szWord.length();
 	m_szTextBuffer.remove(m_iCursorPosition,szWord.length());
-	m_szTextBuffer.insert(m_iCursorPosition,szRreplacement);
+	m_szTextBuffer.insert(m_iCursorPosition,szReplacement);
 	m_szTextBuffer.truncate(m_iMaxBufferSize);
-	moveCursorTo(m_iCursorPosition + szRreplacement.length());
-	if(bRepaint)repaintWithCursorOn();
+	moveCursorTo(m_iCursorPosition + szReplacement.length());
+	if(bRepaint)
+		repaintWithCursorOn();
 }
 
-void KviInputEditor::standardNickCompletion(bool bAddMask, QString & szWord, bool bFirstWordInLine)
+void KviInputEditor::standardNickCompletionInsertCompletedText(const QString &szReplacedWord,const QString &szCompletedText,bool bFirstWordInLine,bool bInCommand)
+{
+	QString szBuffer = szCompletedText;
+
+	if(!KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix).isEmpty())
+	{
+		if(bFirstWordInLine || (!KVI_OPTION_BOOL(KviOption_boolUseNickCompletionPostfixForFirstWordOnly)))
+			szBuffer.append(KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix));
+	}
+	if(bInCommand)
+	{
+		// escape crazy things like Nick\nquit
+		szBuffer.replace(QChar('\\'),QString::fromAscii("\\\\"));
+	}
+	replaceWordBeforeCursor(szReplacedWord,szBuffer,false);
+}
+
+void KviInputEditor::standardNickCompletion(bool bAddMask,QString & szWord,bool bFirstWordInLine,bool bInCommand)
 {
 	// FIXME: this could be really simplified...
-	if(!m_pUserListView) return;
+	if(!m_pUserListView)
+		return;
+
 	selectOneChar(-1);
 
 	QString szBuffer;
 	if(m_szLastCompletedNick.isEmpty())
 	{
 		// New completion session: we NEED sth to complete
-		if(szWord.isEmpty()) return;
+		if(szWord.isEmpty())
+			return;
+
 		if(m_pUserListView->completeNickStandard(szWord,m_szLastCompletedNick,szBuffer,bAddMask))
 		{
 			// completed: save the buffer
@@ -1914,16 +1943,16 @@ void KviInputEditor::standardNickCompletion(bool bAddMask, QString & szWord, boo
 			m_iLastCompletionCursorXPosition  = m_iLastCursorXPosition;
 			m_iLastCompletionFirstVisibleChar = m_iFirstVisibleChar;
 			m_szLastCompletedNick             = szBuffer;
-			if(!KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix).isEmpty())
-			{
-				if(bFirstWordInLine || (!KVI_OPTION_BOOL(KviOption_boolUseNickCompletionPostfixForFirstWordOnly)))
-					szBuffer.append(KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix));
-			}
-			replaceWordBeforeCursor(szWord,szBuffer,false);
+			standardNickCompletionInsertCompletedText(szWord,szBuffer,bFirstWordInLine,bInCommand);
 			m_bLastCompletionFinished=0;
 			// REPAINT CALLED FROM OUTSIDE!
 		} // else no match at all
-	} else  if(!m_bLastCompletionFinished) {
+		
+		return;
+	}
+	
+	if(!m_bLastCompletionFinished)
+	{
 		// Old session
 		// swap the buffers
 		m_szTextBuffer                        = m_szLastCompletionBuffer;
@@ -1939,44 +1968,36 @@ void KviInputEditor::standardNickCompletion(bool bAddMask, QString & szWord, boo
 		{
 			// completed
 			m_szLastCompletedNick             = szBuffer;
-			if(!KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix).isEmpty())
-			{
-				if(bFirstWordInLine || (!KVI_OPTION_BOOL(KviOption_boolUseNickCompletionPostfixForFirstWordOnly)))
-					szBuffer.append(KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix));
-			}
-			replaceWordBeforeCursor(szWord,szBuffer,false);
+			standardNickCompletionInsertCompletedText(szWord,szBuffer,bFirstWordInLine,bInCommand);
 			m_bLastCompletionFinished=0;
 			// REPAINT CALLED FROM OUTSIDE!
 		} else {
 			m_bLastCompletionFinished=1;
 			m_szLastCompletedNick = "";
 		}
+		
+		return;
+	}
+	
+	// Old session finished
+	// re-extract
+	//word = m_szTextBuffer.left(m_iCursorPosition);
+	//getWordBeforeCursor(word,&bFirstWordInLine);
+	if(szWord.isEmpty())return;
+	if(m_pUserListView->completeNickStandard(szWord,"",szBuffer,bAddMask))
+	{
+		// completed
+		m_szLastCompletionBuffer          = m_szTextBuffer;
+		m_iLastCompletionCursorPosition   = m_iCursorPosition;
+		m_iLastCompletionCursorXPosition  = m_iLastCursorXPosition;
+		m_iLastCompletionFirstVisibleChar = m_iFirstVisibleChar;
+		m_szLastCompletedNick             = szBuffer;
+		standardNickCompletionInsertCompletedText(szWord,szBuffer,bFirstWordInLine,bInCommand);
+		m_bLastCompletionFinished=0;
+		// REPAINT CALLED FROM OUTSIDE!
 	} else {
-		// Old session finished
-		// re-extract
-		//word = m_szTextBuffer.left(m_iCursorPosition);
-		//getWordBeforeCursor(word,&bFirstWordInLine);
-		if(szWord.isEmpty())return;
-		if(m_pUserListView->completeNickStandard(szWord,"",szBuffer,bAddMask))
-		{
-			// completed
-			m_szLastCompletionBuffer          = m_szTextBuffer;
-			m_iLastCompletionCursorPosition   = m_iCursorPosition;
-			m_iLastCompletionCursorXPosition  = m_iLastCursorXPosition;
-			m_iLastCompletionFirstVisibleChar = m_iFirstVisibleChar;
-			m_szLastCompletedNick             = szBuffer;
-			if(!KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix).isEmpty())
-			{
-				if(bFirstWordInLine || (!KVI_OPTION_BOOL(KviOption_boolUseNickCompletionPostfixForFirstWordOnly)))
-					szBuffer.append(KVI_OPTION_STRING(KviOption_stringNickCompletionPostfix));
-			}
-			replaceWordBeforeCursor(szWord,szBuffer,false);
-			m_bLastCompletionFinished=0;
-			// REPAINT CALLED FROM OUTSIDE!
-		} else {
-			m_bLastCompletionFinished=1;
-			m_szLastCompletedNick = "";
-		}
+		m_bLastCompletionFinished=1;
+		m_szLastCompletedNick = "";
 	}
 }
 

src/kvirc/ui/kvi_input_editor.h
@@ -383,13 +383,19 @@ private:
 	void completion(bool bShift);
 
 	/**
-	* \brief Completes the nickname
+	* \brief Completes the nickname specified by szWord (inserts the completed text to the input line)
 	* \param bAddMask Whether to complete with the mask of the nickname
 	* \param szWord The nickname to complete
-	* \param bFirstWordInLine Whether to add the suffix from option panel
+	* \param bFirstWordInLine Whether the word to complete is the first word in the text line (so the suffix from option panel is added)
+	* \param bInCommand true if the completion happens inside a kvs command (and thus the completed word should be kvs-escaped)
 	* \return void
 	*/
-	void standardNickCompletion(bool bAddMask, QString & szWord, bool bFirstWordInLine);
+	void standardNickCompletion(bool bAddMask, QString & szWord, bool bFirstWordInLine, bool bInCommand);
+
+	/**
+	* Internal helper for standardNickCompletion()
+	*/
+	void standardNickCompletionInsertCompletedText(const QString &szReplacedWord,const QString &szCompletedText,bool bFirstWordInLine,bool bInCommand);
 
 	/**
 	* \brief Moves the cursor one character to the right
@@ -438,26 +444,23 @@ private:
 	inline static QFontMetrics * getLastFontMetrics(const QFont & font)
 	{
 		if(g_pLastFontMetrics)
+			return g_pLastFontMetrics;
+		g_pLastFontMetrics = new QFontMetrics(font);
+		unsigned short u;
+		for(u=1; u<32; u++)
 		{
-			return g_pLastFontMetrics;
-		} else {
-			g_pLastFontMetrics = new QFontMetrics(font);
-			unsigned short u;
-			for(u=1; u<32; u++)
-			{
-				QChar c = getSubstituteChar(u);
-				KviInputEditor::g_iInputFontCharWidth[u] = g_pLastFontMetrics->width(c);
-				if(c != QChar(u))
-					KviInputEditor::g_iInputFontCharWidth[u] += 4;
-			}
-
-			for(u=32; u<256; u++)
-			{
-				KviInputEditor::g_iInputFontCharWidth[u] = g_pLastFontMetrics->width(QChar(u));
-			}
-
-			return g_pLastFontMetrics;
+			QChar c = getSubstituteChar(u);
+			KviInputEditor::g_iInputFontCharWidth[u] = g_pLastFontMetrics->width(c);
+			if(c != QChar(u))
+				KviInputEditor::g_iInputFontCharWidth[u] += 4;
 		}
+
+		for(u=32; u<256; u++)
+		{
+			KviInputEditor::g_iInputFontCharWidth[u] = g_pLastFontMetrics->width(QChar(u));
+		}
+
+		return g_pLastFontMetrics;
 	}
 
 public slots:

src/kvirc/ui/kvi_ircview.cpp
@@ -630,7 +630,7 @@ void KviIrcView::appendLine(KviIrcViewLine *ptr,bool bRepaint)
 		// a slave view has no log files!
 		if(KVI_OPTION_MSGTYPE(ptr->iMsgType).logEnabled())
 		{
-			add2Log(ptr->szText,ptr->iMsgType);
+			add2Log(ptr->szText,ptr->iMsgType,false);
 			// If we fail...this has been already reported!
 		}
 		// mmh.. when this overflows... we have problems (find doesn't work anymore :()
@@ -650,9 +650,7 @@ void KviIrcView::appendLine(KviIrcViewLine *ptr,bool bRepaint)
 			if(m_pMasterView->m_pLogFile && KVI_OPTION_BOOL(KviOption_boolStripControlCodesInLogs))
 			{
 				if(KVI_OPTION_MSGTYPE(ptr->iMsgType).logEnabled())
-				{
-					m_pMasterView->add2Log(ptr->szText,ptr->iMsgType);
-				}
+					m_pMasterView->add2Log(ptr->szText,ptr->iMsgType,false);
 			}
 			ptr->uIndex = m_pMasterView->m_uNextLineIndex;
 			m_pMasterView->m_uNextLineIndex++;
@@ -2481,12 +2479,14 @@ KviIrcViewWrappedBlock * KviIrcView::getLinkUnderMouse(int xPos,int yPos,QRect *
 				if(iTop != firstRowTop)
 					if(KVI_OPTION_BOOL(KviOption_boolIrcViewWrapMargin))iLeft+=m_iWrapMargin;
 
-				if(xPos < iLeft) return 0; // Mouse is out of this row boundaries
+				if(xPos < iLeft)
+					return 0; // Mouse is out of this row boundaries
 				for(;;)
 				{
 					int iLastLeft = iLeft;
 					//we've run till the end of the line, go away
-					if(i >= l->iBlockCount)return 0;
+					if(i >= l->iBlockCount)
+						return 0;
 					//we try to save the position of the last "text escape" tag we find
 					if(l->pBlocks[i].pChunk)
 						if(l->pBlocks[i].pChunk->type == KVI_TEXT_ESCAPE)

src/kvirc/ui/kvi_ircview.h
@@ -173,8 +173,7 @@ public:
 	void stopLogging();
 	bool isLogging(){ return (m_pLogFile != 0); };
 	void getLogFileName(QString &buffer);
-	//void add2Log(const char *buffer,int buf_len=-1);
-	void add2Log(const QString &szBuffer,int iMsgType=0);
+	void add2Log(const QString &szBuffer,int iMsgType = 0,bool bPrependDate = false);
 
 	//==============================================================================================
 	// Channel view splitting

src/kvirc/ui/kvi_ircview_gettextline.cpp
@@ -69,6 +69,7 @@ const kvi_wchar_t * KviIrcView::getTextLine(int iMsgType,
 					bool bEnableTimeStamp)
 {
 	const kvi_wchar_t* pUnEscapeAt = 0;
+
 	// Splits the text data in lines (separated by '\n')
 
 	// NOTE: This function may be NOT reentrant
@@ -413,6 +414,12 @@ check_escape_switch:
 found_end_of_buffer:
 #endif //COMPILE_USE_DYNAMIC_LABELS
 			APPEND_LAST_TEXT_BLOCK(data_ptr,p - data_ptr)
+			if(pUnEscapeAt)
+			{
+				// empty unescape block needed
+				NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE);
+				// no need to append more data
+			}
 			return p;
 			break;
 		case '\n':
@@ -420,7 +427,13 @@ found_end_of_buffer:
 found_end_of_line:
 #endif //COMPILE_USE_DYNAMIC_LABELS
 			// Found the end of a line
-			APPEND_LAST_TEXT_BLOCK(data_ptr,p - data_ptr)
+			APPEND_LAST_TEXT_BLOCK(data_ptr,p - data_ptr);
+			if(pUnEscapeAt)
+			{
+				// empty unescape block needed
+				NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE);
+				// no need to append more data
+			}
 			// terminate the string
 			// move the current pointer to the next character...
 			// if it is '\0' we will simply stop
@@ -431,7 +444,10 @@ found_end_of_line:
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 found_command_escape:
 #endif //COMPILE_USE_DYNAMIC_LABELS
-			if (p==pUnEscapeAt) {
+
+			if(p == pUnEscapeAt)
+			{
+				// This is the terminator of an escape
 				APPEND_LAST_TEXT_BLOCK(data_ptr,p - data_ptr);
 				NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE);
 				pUnEscapeAt = 0;
@@ -439,6 +455,7 @@ found_command_escape:
 				data_ptr=p;
 				break;
 			}
+
 			// Command escape sequence
 			// \r!<escape_cmd>\r<visible parameters string>\r
 			p++;
@@ -446,12 +463,16 @@ found_command_escape:
 			{
 				const kvi_wchar_t * next_cr = p;
 				// lookup the next carriage return
-				while(*next_cr && (*next_cr != '\r'))next_cr++;
+				while(*next_cr && (*next_cr != '\r'))
+					next_cr++;
+
 				if(*next_cr)
 				{
 					const kvi_wchar_t * term_cr = next_cr;
 					term_cr++;
-					while(*term_cr && (*term_cr != '\r'))term_cr++;
+					while(*term_cr && (*term_cr != '\r'))
+						term_cr++;
+
 					if(*term_cr)
 					{
 						// ok....the format is right:
@@ -473,9 +494,7 @@ found_command_escape:
 
 						++next_cr; //point after the middle \r
 
-						pUnEscapeAt = term_cr;
-
-						bool bColorSetted=false;
+						bool bColorSet=false;
 						if((line_ptr->pChunks[iCurChunk].szPayload[0]=='n') && KVI_OPTION_BOOL(KviOption_boolUseUserListColorsAsNickColors) && (!KVI_OPTION_BOOL(KviOption_boolColorNicks)))
 						{
 							if(m_pKviWindow->type()==KVI_WINDOW_TYPE_CHANNEL && m_pKviWindow)
@@ -486,28 +505,36 @@ found_command_escape:
 									if(e)
 									{
 										line_ptr->pChunks[iCurChunk].colors.fore = KVI_COLOR_CUSTOM; e->color(line_ptr->pChunks[iCurChunk].customFore);
-										bColorSetted=true;
+										bColorSet=true;
 									}
 								}
 							}
 							else if(m_pKviWindow->type()==KVI_WINDOW_TYPE_QUERY && m_pKviWindow && line_ptr->pChunks[iCurChunk].szPayload[1]=='c')
 							{
 								QString m_szNick = QString((QChar*)next_cr,term_cr-next_cr);
-								if(m_szNick==m_pKviWindow->connection()->currentNickName()) {
+								if(m_szNick==m_pKviWindow->connection()->currentNickName())
+								{
 									line_ptr->pChunks[iCurChunk].colors.fore = KVI_COLOR_OWN;
-									bColorSetted=true;
+									bColorSet=true;
 								}
 							}
 						}
-						if(!bColorSetted)
-						{
+						if(!bColorSet)
 							line_ptr->pChunks[iCurChunk].colors.fore=KVI_NOCHANGE;
-						}
 
-						/*APPEND_LAST_TEXT_BLOCK(next_cr,term_cr - next_cr)
-						NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE)*/
+#if 0
+						APPEND_LAST_TEXT_BLOCK(next_cr,term_cr - next_cr)
+						NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE)
+
+						p=term_cr;
+#else
+						pUnEscapeAt = term_cr;
+
+						//APPEND_LAST_TEXT_BLOCK(next_cr,term_cr - next_cr)
+						//NEW_LINE_CHUNK(KVI_TEXT_UNESCAPE)
 
 						p=next_cr;
+#endif
 						data_ptr=p;
 					}
 				}
@@ -607,37 +634,37 @@ found_mirc_escape:
 
 check_http_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	/*
-	 * Profane description: we found an 'h' using the "jump/check table", now check for a 't' (we don't want to search directly for the
-	 * "http://" tag, it takes us more cpu time)
-	 */
-
-	//
-	if((*p == 't') || (*p == 'T'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
 		/*
-		 * Profane description: we found it! now there's an high probability we're in front of an http url. Relax, rewind the last
-		 * character and try to match the complete url protocol tag
- 		 */
-		p--;
-		if(kvi_hstrEqualCIN(p,"http://",7))
+		 * Profane description: we found an 'h' using the "jump/check table", now check for a 't' (we don't want to search directly for the
+		 * "http://" tag, it takes us more cpu time)
+		 */
+	
+		//
+		if((*p == 't') || (*p == 'T'))
 		{
-			partLen = 7;
 			/*
-			* Profane description: we got it; save in partLen the length of the tag and jump to the got_url label.
-			*/
-			goto got_url;
+			 * Profane description: we found it! now there's an high probability we're in front of an http url. Relax, rewind the last
+			 * character and try to match the complete url protocol tag
+	 		 */
+			p--;
+			if(kvi_hstrEqualCIN(p,"http://",7))
+			{
+				partLen = 7;
+				/*
+				* Profane description: we got it; save in partLen the length of the tag and jump to the got_url label.
+				*/
+				goto got_url;
+			}
+			if(kvi_hstrEqualCIN(p,"https://",8))
+			{
+				partLen = 8;
+				goto got_url;
+			}
+			p++;
 		}
-		if(kvi_hstrEqualCIN(p,"https://",8))
-		{
-			partLen = 8;
-			goto got_url;
-		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -647,33 +674,33 @@ if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 
 check_file_or_ftp_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	if((*p == 'i') || (*p == 'I'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"file://",7))
+		if((*p == 'i') || (*p == 'I'))
 		{
-			partLen = 7;
-			goto got_url;
-		}
-		p++;
-	} else if((*p == 't') || (*p == 'T'))
-	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"ftp://",6))
+			p--;
+			if(kvi_hstrEqualCIN(p,"file://",7))
+			{
+				partLen = 7;
+				goto got_url;
+			}
+			p++;
+		} else if((*p == 't') || (*p == 'T'))
 		{
-			partLen = 6;
-			goto got_url;
+			p--;
+			if(kvi_hstrEqualCIN(p,"ftp://",6))
+			{
+				partLen = 6;
+				goto got_url;
+			}
+			if(kvi_hstrEqualCIN(p,"ftp.",4))
+			{
+				partLen = 4;
+				goto got_url;
+			}
+			p++;
 		}
-		if(kvi_hstrEqualCIN(p,"ftp.",4))
-		{
-			partLen = 4;
-			goto got_url;
-		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -682,19 +709,19 @@ if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 
 check_e2k_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	if((*p == 'd') || (*p == 'D'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"ed2k://",7))
+		if((*p == 'd') || (*p == 'D'))
 		{
-			partLen = 7;
-			goto got_url;
+			p--;
+			if(kvi_hstrEqualCIN(p,"ed2k://",7))
+			{
+				partLen = 7;
+				goto got_url;
+			}
+			p++;
 		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -703,19 +730,19 @@ if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 
 check_www_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	if((*p == 'w') || (*p == 'W'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"www.",4))
+		if((*p == 'w') || (*p == 'W'))
 		{
-			partLen = 4;
-			goto got_url;
+			p--;
+			if(kvi_hstrEqualCIN(p,"www.",4))
+			{
+				partLen = 4;
+				goto got_url;
+			}
+			p++;
 		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -724,34 +751,34 @@ if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 
 check_irc_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	if((*p == 'r') || (*p == 'R'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"irc://",6))
+		if((*p == 'r') || (*p == 'R'))
 		{
-			partLen = 6;
-			goto got_url;
+			p--;
+			if(kvi_hstrEqualCIN(p,"irc://",6))
+			{
+				partLen = 6;
+				goto got_url;
+			}
+			if(kvi_hstrEqualCIN(p,"irc6://",7))
+			{
+				partLen = 7;
+				goto got_url;
+			}
+			if(kvi_hstrEqualCIN(p,"ircs://",7))
+			{
+				partLen = 7;
+				goto got_url;
+			}
+			if(kvi_hstrEqualCIN(p,"ircs6://",8))
+			{
+				partLen = 8;
+				goto got_url;
+			}
+			p++;
 		}
-		if(kvi_hstrEqualCIN(p,"irc6://",7))
-		{
-			partLen = 7;
-			goto got_url;
-		}
-		if(kvi_hstrEqualCIN(p,"ircs://",7))
-		{
-			partLen = 7;
-			goto got_url;
-		}
-		if(kvi_hstrEqualCIN(p,"ircs6://",8))
-		{
-			partLen = 8;
-			goto got_url;
-		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -760,19 +787,19 @@ if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 
 check_mailto_url:
 	p++;
-if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
-{
-	if((*p == 'a') || (*p == 'A'))
+	if(KVI_OPTION_BOOL(KviOption_boolIrcViewUrlHighlighting))
 	{
-		p--;
-		if(kvi_hstrEqualCIN(p,"mailto:",7))
+		if((*p == 'a') || (*p == 'A'))
 		{
-			partLen = 7;
-			goto got_url;
+			p--;
+			if(kvi_hstrEqualCIN(p,"mailto:",7))
+			{
+				partLen = 7;
+				goto got_url;
+			}
+			p++;
 		}
-		p++;
 	}
-}
 #ifdef COMPILE_USE_DYNAMIC_LABELS
 	goto *loop_begin;
 #else // !COMPILE_USE_DYNAMIC_LABELS
@@ -983,12 +1010,28 @@ check_emoticon_char:
 
 }
 
+
+
 void KviIrcView::appendText(int iMsgType,const kvi_wchar_t *data_ptr,int iFlags)
 {
 	//appends a text string to the buffer list
 	//splits the lines
 	__range_valid(data_ptr);
 	m_pLastLinkUnderMouse = 0;
+
+	//qDebug("APP [[%s]] PPA",QString::fromUtf16(data_ptr).replace(QChar('\r'),QString::fromAscii("<r>")).toUtf8().data());
+
+	if(!KVI_OPTION_BOOL(KviOption_boolStripControlCodesInLogs))
+	{
+		// Looks like the user wants to keep the control codes in the log file: we just dump everything inside (including newlines...)
+		if(m_pLogFile && KVI_OPTION_MSGTYPE(iMsgType).logEnabled())
+		{
+			add2Log(QString::fromUtf16(data_ptr),iMsgType,true);
+		} else if(m_pMasterView) {
+			if(m_pMasterView->m_pLogFile && KVI_OPTION_MSGTYPE(iMsgType).logEnabled())
+				m_pMasterView->add2Log(QString::fromUtf16(data_ptr),iMsgType,true);
+		}
+	}
 
 	while(*data_ptr)
 	{                                         //Have more data
@@ -998,38 +1041,10 @@ void KviIrcView::appendText(int iMsgType,const kvi_wchar_t *data_ptr,int iFlags)
 		line_ptr->iBlockCount=0;
 		line_ptr->uLineWraps=0;
 
-		if(!KVI_OPTION_BOOL(KviOption_boolStripControlCodesInLogs))
-		{
-			QString szBuffer, szDate;
-			
-			QDateTime date = QDateTime::currentDateTime();
-			switch(KVI_OPTION_UINT(KviOption_uintOutputDatetimeFormat))
-			{
-				case 0:
-					szDate = date.toString("[hh:mm:ss] ");
-					break;
-				case 1:
-					szDate = date.toString(Qt::ISODate);
-					break;
-				case 2:
-					szDate = date.toString(Qt::SystemLocaleDate);
-					break;
-			}
-			kvi_appendWCharToQStringWithLength(&szBuffer,data_ptr,kvi_wstrlen(data_ptr));
-			szBuffer.prepend(szDate);
-			if(m_pLogFile && KVI_OPTION_MSGTYPE(iMsgType).logEnabled())
-			{
-				add2Log(szBuffer,iMsgType);
-			} else if(m_pMasterView) {
-				if(m_pMasterView->m_pLogFile && KVI_OPTION_MSGTYPE(iMsgType).logEnabled())
-				{
-					m_pMasterView->add2Log(szBuffer,iMsgType);
-				}
-			}
-		}
+		data_ptr = getTextLine(iMsgType,data_ptr,line_ptr,!(iFlags & NoTimestamp));
 
-		data_ptr=getTextLine(iMsgType,data_ptr,line_ptr,!(iFlags & NoTimestamp));
 		appendLine(line_ptr,!(iFlags & NoRepaint));
+
 		if(iFlags & SetLineMark)
 		{
 			if(KVI_OPTION_BOOL(KviOption_boolTrackLastReadTextViewLine))

src/kvirc/ui/kvi_window.cpp
@@ -576,7 +576,8 @@ void KviWindow::saveProperties(KviConfig *cfg)
 	QString szKey = "TextEncoding_";
 	szKey += m_szName;
 	cfg->writeEntry(szKey,szCodec);
-	if(m_pInput) {
+	if(m_pInput)
+	{
 		cfg->writeEntry("inputToolButtonsHidden",m_pInput->isButtonsHidden());
 		cfg->writeEntry("commandLineIsUserFriendly",m_pInput->isUserFriendly());
 	}
@@ -593,7 +594,8 @@ void KviWindow::loadProperties(KviConfig *cfg)
 	QString szKey = "TextEncoding_";
 	szKey += m_szName;
 	setTextEncoding(cfg->readQStringEntry(szKey,KviQString::Empty).toUtf8().data());
-	if(m_pInput) {
+	if(m_pInput)
+	{
 		m_pInput->setButtonsHidden(cfg->readBoolEntry("inputToolButtonsHidden",KVI_OPTION_BOOL(KviOption_boolHideInputToolButtons)));
 		m_pInput->setUserFriendly(cfg->readBoolEntry("commandLineIsUserFriendly",KVI_OPTION_BOOL(KviOption_boolCommandlineInUserFriendlyModeByDefault)));
 	}

commit 59c03812edddce2b03595d68d3e31786d5cbfcf2

fix for a regression in log saving introduced in r4265
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4313 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/ui/kvi_ircview_loghandling.cpp
@@ -253,7 +253,7 @@ void KviIrcView::add2Log(const QString &szBuffer,int iMsgType,bool bPrependDate)
 
 	if(!KVI_OPTION_BOOL(KviOption_boolStripMsgTypeInLogs))
 	{
-		QString szMessageType = QString("%1 \n").arg(iMsgType);
+		QString szMessageType = QString("%1 ").arg(iMsgType);
 
 		szTmp = KviQString::toUtf8(szMessageType);
 
@@ -285,6 +285,7 @@ void KviIrcView::add2Log(const QString &szBuffer,int iMsgType,bool bPrependDate)
 	}
 	
 	szTmp = KviQString::toUtf8(szBuffer);
+	szTmp.append('\n');
 
 	if(m_pLogFile->write(szTmp.data(),szTmp.length())==-1)
 		debug("WARNING : Can not write to the log file.");